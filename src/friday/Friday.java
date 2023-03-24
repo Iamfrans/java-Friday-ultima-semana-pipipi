f0a9ae28d4c8558481b383272d2636b4b2958d39
iamfrans <1323544@senati.pe>
2023-03-24 09:24:33 -0500

    Realizando el problema

---
+++ src/friday/Friday.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package friday;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Friday {
+
+    /**
+     * @param args the command line arguments
+     */
+        public static void main(String[] args) {
+        System.out.println("   J    A     V     V    A   ");
+        System.out.println("   J   A A     V   V    A A  ");
+        System.out.println("J  J  AAAAA     V V    AAAAA ");
+        System.out.println(" JJ  A     A     V    A     A");
+    }
+}
